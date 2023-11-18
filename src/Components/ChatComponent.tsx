045db189a54e01c83b5f3cae8877aa9de8205ecf
YINA YI <yinayi@YINAs-MacBook-Pro.local>
2023-11-18 15:45:02 -0800

    Auto scroll down to end

--- src/Components/ChatComponent.tsx
+++ src/Components/ChatComponent.tsx
@@ -6,23 +6,19 @@ import ChatClient from "../Engine/ChatClient";
 import './ChatStyles.css'
 import e from "express";
 
-
-
-
-
 const chatClient = new ChatClient();
-
 
 function ChatComponent() {
     const [messages, setMessages] = useState<MessageContainer[]>([]);
-    const [mostRecentId, setMostRecentId] = useState<number>(0);
+    const [mostRecentId, setMostRecentId] = useState<number>(-1);
     const [user, setUser] = useState(window.sessionStorage.getItem('userName') || "");
     const [message, setMessage] = useState<string>("");
-    const bottomRef = useRef(null);
-
+    const bottomRef = useRef<HTMLDivElement>(null);;
+    const [lastMessageCount, setLastMessageCount] = useState(0);
 
     let localUser = user;
     let localMessage = message;
+    
     const updateDisplay = useCallback(() => {
         let updateNeeded = false;
         const newLastId = chatClient.messages[0].id;
@@ -47,12 +43,22 @@ function ChatComponent() {
         chatClient.setCallback(updateDisplay);
     }, [updateDisplay]);
 
-
+    useEffect(() => {
+        const messageBox = document.querySelector('.message-box');
+        if (messageBox && messages.length > lastMessageCount) {
+            messageBox.scrollTop = messageBox.scrollHeight;
+        }
+        setLastMessageCount(messages.length);
+    }, [messages, lastMessageCount]);
+    
+    
     function makeFormatedMessages() {
         return [...messages].reverse().map((message, index) => {
             let isSender = message.user === localUser;
             let messageWrapperClass = isSender ? "message-wrapper sender" : "message-wrapper receiver";
     
+            const ref = index === 0 ? bottomRef : null;
+
             return (
                 <div key={index} className={messageWrapperClass}>
                     {!isSender && <div className="message-user">{message.user}</div>}
@@ -65,7 +71,7 @@ function ChatComponent() {
 
     function handleSendMessage() {
         chatClient.sendMessage(localUser, message);
-        setMessage("");  // 清空输入框
+        setMessage(""); // clear the message box
     }
 
     return (
